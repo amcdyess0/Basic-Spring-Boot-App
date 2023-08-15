c2a18f9a3c40d05bbba2b50f5caa2d4b6f2139ff
amcdyess0 <aquonmcdyess091@gmail.com>
2023-08-15 15:44:55 -0400

    organization

--- src/main/java/com/website/website/model/User.java
+++ src/main/java/com/website/website/model/User.java
@@ -13,7 +13,7 @@ import org.springframework.data.mongodb.core.mapping.Field;
 @Getter
 @Setter
 @ToString
-@Document(collection = "users")
+@Document(collection = "")
 public class User {
     @Id
     @Field(name = "_id")
